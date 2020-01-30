398e8c703fddb5dc7860e1a8310d34ad20c72a75
Jacob W Jones <jacobwadejones76@gmail.com>
2020-01-30 19:20:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,6 @@ def print_business_card(person)
         Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
-
+bob  = ["Bob", 46, "Baker", "555-555-5555"]
+steph  = ["Steph", 28, "CEO" "222-222-2222"]
 
