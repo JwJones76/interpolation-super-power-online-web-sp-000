599972ad146488236c25b9f633aaad73598ac822
Jacob W Jones <jacobwadejones76@gmail.com>
2020-01-30 19:17:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
-# Write your #display_rainbow method here
+def print_business_card(person)
+  puts "name: #{person[0]}, 
+        Age: #{person[1]}, 
+        Occupation: #{person[2]}"
+  puts "Contact: #{person[3]}"
+  
+
+end
